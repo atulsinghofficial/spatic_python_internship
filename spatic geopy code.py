c71f24231c62e9704898c8f7d0697c79f4904533
Atul Singh <atulsinghraj163@gmail.com>
2023-02-15 19:38:16 +0530

    Update spatic geopy code.py

--- spatic geopy code.py
+++ spatic geopy code.py
@@ -1,40 +1,43 @@
-from geopy.distance import distance
-from fuzzywuzzy import fuzz
 import pandas as pd
+from math import radians, sin, cos, sqrt, asin
+import Levenshtein
+
 
 # Read in the dataset
 df = pd.read_csv('data.csv')
 
 
-# Define a function to check if two entries are within 200 meters of each other
-def is_close(coord1, coord2):
-    return distance(coord1, coord2).m < 200
+# haversine formula determines the great-circle distance between two points on a sphere given their longitudes and latitudes.
+def haversine(lat1, lon1, lat2, lon2):
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)2 + cos(lat1) * cos(lat2) * sin(dlon/2)2
+    c = 2 * asin(sqrt(a))
+    r = 6371
+    return c * r * 1000
 
 
 # Define a function to check if two names are similar
-def is_similar(name1, name2):
-    return fuzz.token_sort_ratio(name1, name2) > 80
+def is_similar(s1, s2):
+    distance = Levenshtein.distance(s1.lower(), s2.lower())
+    return distance <= 5
 
 
 # Add a new column to the DataFrame to store the similarity results
-df['is_similar'] = False
+df['is_similar'] = 0
 
 
 # Loop through all pairs of entries in the dataset
-for i in range(len(df)):
-    for j in range(i+1, len(df)):
-        
-        # Check if the entries are within 200 meters and have similar names
-        if is_close((df.iloc[i]['latitude'], df.iloc[i]['longitude']),
-                    (df.iloc[j]['latitude'], df.iloc[j]['longitude'])) and \
-           is_similar(df.iloc[i]['name'], df.iloc[j]['name']):
-            
-            # Mark the entries as similar in the DataFrame
-            df.at[i, 'is_similar'] = True
-            df.at[j, 'is_similar'] = True
-    print(i)
-    if i > 5:
-        break
+for i, row1 in df.iterrows():
+    for j, row2 in df.iterrows():
+        if i != j:
+            if haversine(row1['latitude'], row1['longitude'], row2['latitude'], row2['longitude']) <= 200:
+                if is_similar(row1['name'], row2['name']):
+                    df.loc[i, 'is_similar'] = 1
+                    df.loc[j, 'is_similar'] = 1
+
+
 
         # Write the DataFrame to a new CSV file
 df.to_csv('data_out.csv', index=False)
